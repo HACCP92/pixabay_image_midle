819323fcc0ab5017e0aa286fbe2e3e0f3b3b1d63
HACCP92 <111282517+HACCP92@users.noreply.github.com>
2023-07-13 06:35:39 +0900

    [이미지 검색 앱] detail screen ui 슈정 및 구현

---
+++ lib/presentation/detail/detail_screen.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+import 'package:pixabay_image_midle/domain/model/photo.dart';
+
+class DetailScreen extends StatelessWidget {
+  final Photo photo;
+  const DetailScreen({Key? key, required this.photo}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('디테일 페이지'),
+      ),
+      body: Column(
+        children: [
+          Hero(
+            tag: photo.id,
+            child: Image.network(
+              photo.webformatURL,
+              fit: BoxFit.cover,
+            ),
+          ),
+          Text('조회수: ${photo.views}'),
+        ],
+      ),
+    );
+  }
+}
